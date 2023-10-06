b5743cbfb6d98f9766ee67eed2fe62532e91d67e
med <medioqrity@gmail.com>
2023-10-05 23:48:24 -0400

    make discord io input work

--- io_utils/discord_io.py
+++ io_utils/discord_io.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Dict, List, Tuple
 from result import Result, Ok, Err
 from io_utils.abstract_io import AbstractIO
 import discord
@@ -9,37 +9,34 @@ import asyncio
 class DiscordIO(AbstractIO):
     def __init__(
         self,
-        client: discord.Client,
         token: str,
         history_limit=100,
         send_to: Optional[TextChannel] = None,
     ) -> None:
-        print("Init discord IO ...", end="", flush=True)
-        self.client = client
         self.history_limit = history_limit
         self.send_to = send_to
-        asyncio.run(self.client.login(token))
-        print(" done")
-
-    def __del__(self):
-        asyncio.run(self.client.close())
+        self.token = token
 
     def get_history(self) -> str:
         # https://discordpy.readthedocs.io/en/stable/ext/commands/api.html#discord.ext.commands.Context.history
         # TODO: also include guild information
-        async def get_guilds():
-            return [guild async for guild in self.client.fetch_guilds(limit=150)]
-        guilds = asyncio.run(get_guilds())
-        print(guilds)
-        async def get_messages() -> str:
-            messages = [
-                message
-                async for guild in self.client.fetch_guilds(limit=150)
-                for channel in guild.channels if isinstance(channel, discord.TextChannel)
-                async for message in channel.history(limit=self.history_limit)
-            ]
-            return str(messages)
-        return asyncio.run(get_messages())
+        intents = discord.Intents.default()
+        intents.message_content = True
+        bot = discord.Client(intents=intents)
+
+        history: Dict[str, Dict[str, List[Tuple[str, str]]]] = {}
+
+        @bot.event
+        async def on_ready():
+            for guild in bot.guilds:
+                for channel in guild.text_channels:
+                    history.setdefault(guild.name, dict())[channel.name] = [
+                        (message.author.name, message.content) async for message in channel.history(limit=self.history_limit)
+                    ]
+            await bot.close()
+
+        bot.run(self.token)
+        return str(history)
 
     def send(self, summary: str) -> Result[discord.Message, str]:
         if self.send_to is not None:
--- main.py
+++ main.py
@@ -2,19 +2,13 @@ from io_utils.discord_io import DiscordIO
 from io_utils.dummy_io import DummyIO
 from summarizer.identity_summarizer import IdentitySummarizer
 from app import App
-import discord
 
-
-intents = discord.Intents.default()
-intents.message_content = True
-discord_client = discord.Client(intents=intents)
 
 with open("discord_token", "r") as f:
     token = f.read()
 
 def main():
-    print("main start")
-    dummy_app = App(DiscordIO(discord_client, token), IdentitySummarizer(), DummyIO())
+    dummy_app = App(DiscordIO(token, history_limit=3), IdentitySummarizer(), DummyIO())
     print(dummy_app.summarize())
 
 if __name__ == "__main__":
